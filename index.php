3ecfaebd75b6cf282f43c796e8d9092ac33ff06a
DiPaLu <seiya@windowslive.com>
2016-11-16 14:28:28 +0100

    ajout style

--- index.php
+++ index.php
@@ -71,6 +71,16 @@ $quizzList = array(
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
           integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <style>
+    	body {
+    		background-color: black;
+    		color: white;
+    	}
+
+    	.jumbotron {
+    		font-size: 3em;
+    	}
+    </style>
 </head>
 <body>
 <div role="main" class="container theme-showcase">
